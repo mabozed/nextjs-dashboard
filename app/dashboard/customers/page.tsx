441c18a60b7f11a295cef5e3e7feeef4999f981f
vica web <vicaweb.com@gmail.com>
2024-01-20 16:49:03 +0300

    chapter 4

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+const CustomersPage = () => {
+  return (
+    <div>
+      <p>Customers Page</p>
+    </div>
+  );
+};
+
+export default CustomersPage;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+
+const InvoicesPage = () => {
+  return (
+    <div>
+      <p>Invoices Page</p>
+    </div>
+  );
+}
+
+export default InvoicesPage
